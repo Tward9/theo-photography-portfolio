842c6aa50402b5fbc83acf317355cd2221164847
Theo Ward <theoward9@gmail.com>
2023-01-26 21:43:26 -0600

    Updated Title, playing with spacing in title block

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Camera from '../camera.png';
 
 function Header(){
 
     return(
         <>
+            <div className="headerGroup">
+                <img src={Camera} className='headerPhoto' alt='Camera'/>
+                <h1 className="headerTitle">Life in 35mm</h1>
+                <h5 className="headerTitle">Photography by Theo</h5>
+            </div>
         </>
     )
 };

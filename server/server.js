71c92411cf126a9a5ec6687ea319baa7162a63ce
Theo Ward <theoward9@gmail.com>
2023-01-30 20:24:25 -0600

    setting up mongo, connection and updating server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,42 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3001;
+const path = require('path');
+const mongo = require('./config/connection');
+// const routes = require('./routes');
 
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+var session = require('express-session');
+var MongoDBStore = require('connect-mongodb-session')(session);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const sess = {
+  secret: 'This is a secret',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+  },
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoDBStore({
+    db: mongo
+  }),
+}
+
+app.use(session(sess));
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+// app.use(routes);
+
+mongo.once('open', () => {
+  app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
+});
+
+
 
 app.get('/express_backend', (req, res) => { //Line 9
     res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10

fb2741ac86ad05c388d717ef679f543b819e64fc
Theo Ward <theoward9@gmail.com>
2023-02-06 20:53:00 -0600

    appears to function  as long as adding something doesn't break it  then you need to remove the offending piece  and rerun until is isn't broken

--- src/components/Picture/index.js
+++ src/components/Picture/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Picture = (({ key }) => {
+    console.log(key, 'comming from picture.js');
     return (
         <>
             <img className="imgBox" src={`https://${process.env.REACT_APP_AWS_S3_BUCKET_NAME}.s3.${process.env.REACT_APP_AWS_S3_REGION}.amazonaws.com/${key}`} 

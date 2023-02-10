a773f0368b0b54e0e6a86f29e72907d47fbf7db3
Theo Ward <theoward9@gmail.com>
2023-02-09 21:07:44 -0600

    conditionally assings class name based on image orientation

--- src/components/Picture/index.js
+++ src/components/Picture/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Picture = (({ url }) => {
-    console.log(url, 'comming from picture.js');
+    // const orient = 
     return (
         <>
-            <img className="imgBox" src={`https://${process.env.REACT_APP_AWS_S3_BUCKET_NAME}.s3.${process.env.REACT_APP_AWS_S3_REGION}.amazonaws.com/${url}`} alt="miami harbor" />
+            <img className={url.orient === 'landscape' ? 'imgBoxLandscape' : 'imgBoxPortrait'} src={`https://${process.env.REACT_APP_AWS_S3_BUCKET_NAME}.s3.${process.env.REACT_APP_AWS_S3_REGION}.amazonaws.com/${url.url}`} alt={url.alt} />
         </>
     )
 });
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,70 +6,49 @@ import { useEffect, useState } from "react";
 
 
 function Home() {
-    // const s3 = new AWS.S3({
-    //     accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-    //     signatureVersion: 'v4',
-    //     region: process.env.REACT_APP_AWS_S3_REGION,
-    //     secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
-    // });
-
-    // const hiddenUrl = `https://${process.env.REACT_APP_AWS_S3_BUCKET_NAME}.s3.${process.env.REACT_APP_AWS_S3_REGION}.amazonaws.com/${process.env.REACT_APP_TEST_KEY}`
-
-    // var params = {
-    //     Bucket: `${process.env.REACT_APP_AWS_S3_BUCKET_NAME}`,
-    //     MaxKeys: 10
-    // };
-    // let itemArr = [];
-    // const [photoMount, setPhotoMount] = useState(false);
-
-    // useEffect(() => {
-    //     s3.listObjects(params, function (err, data) {
-    //         if (err) console.log(err, err.stack); // an error occurred
-    //         else {
-    //             let items = data;
-    //             items.Contents.forEach(item => {
-    //                 itemArr.push(item)
-    //             })
-    //             // console.log(itemArr);
-    //         }
-    //     });
-    //     return () => {
-    //         console.log('This will be logged on unmount');
-    //         console.log(itemArr);
-    //         setPhotoMount(true)
-    //     };
-    // });
-    // const picture = 'https://theo-portfolio-bucket.s3.us-east-2.amazonaws.com/003C0484-BDF9-4E83-BF3F-795BB9913093.JPG'
-    const urlArray = [
-        '003C0484-BDF9-4E83-BF3F-795BB9913093.JPG',
-        '57AE55C4-87E1-4F59-A033-D19B5E66C1A9.JPG',
-        'IMG_9585.png',
-        'IMG_9594.png',
-        'IMG_9602.png',
-        'IMG_9612.png'
-    ];
+    
+    const urlObject = [
+        {
+            url: '003C0484-BDF9-4E83-BF3F-795BB9913093.JPG',
+            alt: 'Miami Harbor With Cargo Ship Leaving',
+            orient: 'landscape'
+        },
+        {
+            url: '57AE55C4-87E1-4F59-A033-D19B5E66C1A9.JPG',
+            alt: 'Miami Harbor Sunset',
+            orient: 'landscape'
+        },
+        {
+            url: 'IMG_9585.png',
+            alt: 'Sunset on the Water with Schooner',
+            orient: 'portrait'
+        },
+        {
+            url: 'IMG_9594.png',
+            alt: 'Sunset on the Water',
+            orient: 'portrait'
+        },
+        {
+            url: 'IMG_9602.png',
+            alt: 'Sunset with a Mast',
+            orient: 'portrait'
+        },
+        {
+            url: 'IMG_9612.png',
+            alt: 'Mast, Sail, and Sunset',
+            orient: 'portrait'
+        }
+    ]
     
     return (
         <>
             <div>
                 {
-                    urlArray.map((url) => {
-                        // <img className="imgBox" src={`https://${process.env.REACT_APP_AWS_S3_BUCKET_NAME}.s3.${process.env.REACT_APP_AWS_S3_REGION}.amazonaws.com/${url}`} alt="miami harbor" />
-                        return(<Picture url={url} />)
-                        
-                        
+                    urlObject.map((url) => {
+                        return(<Picture url={url} />)                        
                     })
                 }
             </div>
-            {/* <div>
-                {
-                    photoMount ? (
-                        <PhotoHolder pictures={itemArr} />
-                    ) : (
-                        <div>Loading...</div>
-                    )
-                }
-            </div> */}
         </>
     )
 };

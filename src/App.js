f8a137b21984d738d2fef53fa82f50040b9f509a
Theo Ward <theoward9@gmail.com>
2023-02-13 21:08:59 -0600

    was planning to add image component to look at specific image. First plan wasn't working, might need to be child component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Header from './components/Header';
 import Home from './pages/Home';
+import React from 'react';
 
 function App() {
-  
+
   return (
     <>
       <Header />
-      <Home />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/components/Picture/index.js
+++ src/components/Picture/index.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 const Picture = (({ url }) => {
-    // const orient = 
+
     return (
         <>
 

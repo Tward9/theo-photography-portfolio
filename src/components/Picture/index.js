29be2d3f40d05b08ef403b172fbca541930cdd79
Theo Ward <theoward9@gmail.com>
2023-02-21 19:49:04 -0600

    fixed removing context menu

--- src/components/Picture/index.js
+++ src/components/Picture/index.js
@@ -3,10 +3,10 @@ import React from "react";
 
 
 const Picture = (({ url }) => {
-
+    let prevent = (e)=>{e.preventDefault()};
     return (
         <>
-            <img className={url.orient === 'landscape' ? 'imgBoxLandscape margin' : 'imgBoxPortrait margin'} src={`https://${process.env.REACT_APP_AWS_S3_BUCKET_NAME}.s3.${process.env.REACT_APP_AWS_S3_REGION}.amazonaws.com/${url.url}`} alt={url.alt} onContextMenu="return false;" />
+            <img className={url.orient === 'landscape' ? 'imgBoxLandscape margin' : 'imgBoxPortrait margin'} src={`https://${process.env.REACT_APP_AWS_S3_BUCKET_NAME}.s3.${process.env.REACT_APP_AWS_S3_REGION}.amazonaws.com/${url.url}`} alt={url.alt} onContextMenu={prevent} />
         </>
 
     )
